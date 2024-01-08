8c6ce6ab567a4533eb841b5c4849fb0e5ace453d
gminiy <gmini.y@gmail.com>
2024-01-08 18:54:50 +0900

    feat: main screen 기능 구현

--- lib/presentation/screen/main_screen.dart
+++ lib/presentation/screen/main_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_subway_info_app/presentation/view_model/main_state.dart';
+import 'package:flutter_subway_info_app/presentation/view_model/main_view_model.dart';
+import 'package:provider/provider.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -12,6 +15,9 @@ class _MainScreenState extends State<MainScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final MainViewModel viewModel = context.watch<MainViewModel>();
+    final MainState state = viewModel.state;
+
     return Scaffold(
       body: SafeArea(
         child: Padding(
@@ -24,20 +30,22 @@ class _MainScreenState extends State<MainScreen> {
                     hintText: '역 이름을 입력하세요',
                     suffixIcon: IconButton(
                       icon: const Icon(Icons.search_outlined),
-                      onPressed: () {},
+                      onPressed: () {
+                        viewModel.querySubwayInfo(textEditingController.text);
+                      },
                     )),
               ),
-              const SizedBox(
-                height: 32
-              ),
-              Expanded(
-                child: ListView.builder(
-                  itemCount: 20,
-                  itemBuilder: (context, index) {
-                    return const Text('test');
-                  },
-                ),
-              )
+              const SizedBox(height: 32),
+              state.isLoading
+                  ? const CircularProgressIndicator()
+                  : Expanded(
+                      child: ListView.builder(
+                        itemCount: state.subwayInfoModels.length,
+                        itemBuilder: (context, index) {
+                          return Text(state.subwayInfoModels[index].toString());
+                        },
+                      ),
+                    )
             ],
           ),
         ),
