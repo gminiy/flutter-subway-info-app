a0850ba04278cf59f3970fdfe1680dbb191595ba
gminiy <gmini.y@gmail.com>
2024-01-08 15:49:50 +0900

    fix: subway_info_repository.dart result 리턴 제외. use case에서 에러 처리할 에정

--- lib/domain/repository/subway_info_repository.dart
+++ lib/domain/repository/subway_info_repository.dart
@@ -1,6 +1,5 @@
-import 'package:flutter_subway_info_app/core/result.dart';
 import 'package:flutter_subway_info_app/domain/model/subway_info_model.dart';
 
 abstract interface class SubwayInfoRepository {
-  Future<Result<List<SubwayInfoModel>>> querySubwayInfo(String station);
+  Future<List<SubwayInfoModel>> querySubwayInfo(String station);
 }
