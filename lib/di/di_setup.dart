e62f4175574cfa2640abee299be88215bf0e044e
gminiy <gmini.y@gmail.com>
2024-01-08 18:33:20 +0900

    fix: MainViewModel 의존성 주입

--- lib/di/di_setup.dart
+++ lib/di/di_setup.dart
@@ -2,6 +2,9 @@ import 'package:dio/dio.dart';
 import 'package:flutter_subway_info_app/data/data_source/remote/seoul_subway_info_api.dart';
 import 'package:flutter_subway_info_app/data/data_source/remote/subway_info_api.dart';
 import 'package:flutter_subway_info_app/data/repository/subway_info_repository_impl.dart';
+import 'package:flutter_subway_info_app/domain/repository/subway_info_repository.dart';
+import 'package:flutter_subway_info_app/domain/use_case/query_subway_info_use_case.dart';
+import 'package:flutter_subway_info_app/presentation/view_model/main_view_model.dart';
 import 'package:get_it/get_it.dart';
 
 final getIt = GetIt.instance;
@@ -9,6 +12,10 @@ final getIt = GetIt.instance;
 void diSetup() {
   getIt.registerSingleton<Dio>(Dio());
   getIt.registerSingleton<SubwayInfoApi>(SeoulSubwayInfoApi(dio: getIt<Dio>()));
-  getIt.registerSingleton<SubwayInfoRepositoryImple>(
+  getIt.registerSingleton<SubwayInfoRepository>(
       SubwayInfoRepositoryImple(api: getIt()));
+  getIt.registerSingleton<QuerySubwayInfoUseCase>(QuerySubwayInfoUseCase(
+      subwayInfoRepository: getIt<SubwayInfoRepository>()));
+  getIt.registerFactory<MainViewModel>(() =>
+      MainViewModel(querySubwayInfoUseCase: getIt<QuerySubwayInfoUseCase>()));
 }
