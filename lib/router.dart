182108d6b6dfef31aa68ad4935f8ee04d0799a88
gminiy <gmini.y@gmail.com>
2024-01-08 18:42:49 +0900

    feat: mainViewModel Provider 적용

--- lib/router.dart
+++ lib/router.dart
@@ -1,9 +1,15 @@
+import 'package:flutter_subway_info_app/di/di_setup.dart';
 import 'package:flutter_subway_info_app/presentation/screen/main_screen.dart';
+import 'package:flutter_subway_info_app/presentation/view_model/main_view_model.dart';
 import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
 
 final router = GoRouter(routes: [
   GoRoute(
     path: '/',
-    builder: (context, state) => const MainScreen(),
+    builder: (context, state) => ChangeNotifierProvider(
+      create: (context) => getIt<MainViewModel>(),
+      child: const MainScreen(),
+    ),
   )
 ]);
