82dda64b4e6ae3d737aa41da52707263212e565a
gminiy <gmini.y@gmail.com>
2024-01-08 19:19:07 +0900

    feat: debounce.dart

---
+++ lib/util/debounce.dart
@@ -0,0 +1,15 @@
+import 'dart:async';
+
+class Debounce {
+  final Duration duration;
+  Timer? _timer;
+
+  Debounce(this.duration);
+
+  void onEvent(void Function() callback) {
+    if (_timer?.isActive ?? false) {
+      _timer?.cancel();
+    }
+    _timer = Timer(duration, () => callback());
+  }
+}
